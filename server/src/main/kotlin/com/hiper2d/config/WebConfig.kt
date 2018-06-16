f60088974b803d37a649f16349964097743e220d
hiper2d <hiper2d@gmail.com>
2018-06-16 19:57:37 +0200

    Updated Kotlin to 1.2.50. Added Log4j2 dependencies and config file.

--- server/src/main/kotlin/com/hiper2d/config/WebConfig.kt
+++ server/src/main/kotlin/com/hiper2d/config/WebConfig.kt
@@ -2,19 +2,15 @@ package com.hiper2d.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-open class WebConfig: WebMvcConfigurerAdapter() {
+open class WebConfig: WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
                 .allowedHeaders("*")
                 .allowedOrigins("*")
     }
-
-
 }
 
