ff3e78b8a1e2567568ed4b9f755275493428f4ef
Alexey Zelenovsky <hiper2d@gmail.com>
2018-06-24 21:10:17 -0400

    Made some fixes in Security Config

--- server/src/main/kotlin/com/hiper2d/config/WebConfig.kt
+++ server/src/main/kotlin/com/hiper2d/config/WebConfig.kt
@@ -7,10 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig: WebMvcConfigurer {
 
+    // todo: Understand why it is ignored. Probably some headers should be added to a client
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-                .allowedHeaders("*")
-                .allowedOrigins("*")
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:9002")
     }
 }
 
--- server/src/main/kotlin/com/hiper2d/config/WebSecurityConfig.kt
+++ server/src/main/kotlin/com/hiper2d/config/WebSecurityConfig.kt
@@ -1,6 +1,5 @@
 package com.hiper2d.config
 
-import com.hiper2d.security.handler.CustomLogoutSuccessHandler
 import com.hiper2d.security.provider.AnyAuthenticationProvider
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
@@ -10,39 +9,46 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig(
-        private val customLogoutSuccessHandler: CustomLogoutSuccessHandler,
         private val anyAuthenticationProvider: AnyAuthenticationProvider
 ): WebSecurityConfigurerAdapter() {
 
     override fun configure(auth: AuthenticationManagerBuilder) {
+        // A custom provider which allows to authenticate with any Latin username/password
         auth.authenticationProvider(anyAuthenticationProvider)
     }
 
     override fun configure(http: HttpSecurity) {
         http
-                .httpBasic()
-                .and()
-                .authorizeRequests()
-                    .antMatchers("/").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                .logout()
-                    .logoutRequestMatcher(AntPathRequestMatcher("/api/logout", "POST"))
-                    .logoutSuccessHandler(customLogoutSuccessHandler)
-                .and()
-                .csrf()
-                    .ignoringAntMatchers("/api/login", "/api/logout")
-                .and()
-                .exceptionHandling()
+            .httpBasic()
+            .and()
+            .authorizeRequests()
+                // Necessary when running the application with compiled client as static resources
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+            .and()
+            .logout()
+                .logoutRequestMatcher(AntPathRequestMatcher("/api/logout", "POST"))
+            .and()
+            .csrf()
+                // Allow a JavaScript client to read the XSRF-TOKEN from cookie
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .and()
+                // Use MVC CORS configuration
+                .cors()
+            .and()
+            .exceptionHandling()
+                // An AuthenticationEntryPoint that sends a generic HttpStatus as a response
                 .authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
     }
 
     override fun configure(web: WebSecurity) {
+        // Everything below is necessary when running the application with compiled client as static resources
         web.ignoring()
                 .mvcMatchers("/index.html")
                 .mvcMatchers("/*.css")
